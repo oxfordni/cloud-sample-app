6d5722ecaf0ee8ca31f8f5aee9589dcfcfe76d2d
Ross MacDonald <rossmacdonald@Rosss-Air.lan>
2022-03-05 17:50:28 -0800

    adding github actions

--- frontend/cypress/integration/1-getting-started/serverhealth.spec.js
+++ frontend/cypress/integration/1-getting-started/serverhealth.spec.js
@@ -1,15 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Server Health Display', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3001')
-  })
-
   it('displays green when python server alive', () => {
     cy.intercept('http://localhost:3001/py/health', {
       statusCode: 200,
       body: { alive: 'ok' },
-    })
+    }).as('pyhealth')
+    cy.visit('http://localhost:3001')
+    cy.wait('@pyhealth')
     cy.wait(3000)
     cy.contains('python service').then(($py) => {
       expect($py).to.have.css('color', 'rgb(82, 196, 26)')
@@ -19,7 +17,9 @@ describe('Server Health Display', () => {
     cy.intercept('http://localhost:3001/py/health', {
       statusCode: 504,
       body: '504 Gateway Timeout',
-    })
+    }).as('pyhealth')
+    cy.visit('http://localhost:3001')
+    cy.wait('@pyhealth')
     cy.wait(3000)
     cy.contains('python service').then(($py) => {
       expect($py).to.have.css('color', 'rgb(255, 77, 79)')
@@ -29,7 +29,9 @@ describe('Server Health Display', () => {
     cy.intercept('http://localhost:3001/go/health', {
       statusCode: 200,
       body: { alive: 'ok' },
-    })
+    }).as('gohealth')
+    cy.visit('http://localhost:3001')
+    cy.wait('@gohealth')
     cy.wait(3000)
     cy.contains('go service').then(($go) => {
       expect($go).to.have.css('color', 'rgb(82, 196, 26)')
@@ -39,7 +41,9 @@ describe('Server Health Display', () => {
     cy.intercept('http://localhost:3001/go/health', {
       statusCode: 504,
       body: '504 Gateway Timeout',
-    })
+    }).as('gohealth')
+    cy.visit('http://localhost:3001')
+    cy.wait('@gohealth')
     cy.wait(3000)
     cy.contains('go service').then(($go) => {
       expect($go).to.have.css('color', 'rgb(255, 77, 79)')
